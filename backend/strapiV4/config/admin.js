0cd8b5cd9a986156f37869f9a19443dbd31fba5b
Pankaj Shere <pankaj.shere@mediamonks.com>
2022-03-17 00:42:20 +0530

    redis cache

--- backend/strapiV4/config/admin.js
+++ backend/strapiV4/config/admin.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '6bc4b80126dbfcf4d6534c95931ffd2b'),
   },
-  url: '/',
+  url: 'https://admin.dessertcorner.com',
   serveAdminPanel: false,
 });
---
+++ backend/strapiV4/config/plugins.js
@@ -0,0 +1,36 @@
+module.exports = {
+redis: {
+    config: {
+      connections: {
+        default: {
+          connection: {
+            host: 'redis',
+            port: 6379,
+            db: 0,
+          },
+          settings: {
+            debug: false,
+          },
+        },
+      },
+    },
+  },
+  // Step 2: Configure the redis cache plugin
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "redis",
+        options: {
+          max: 32767,
+          connection: "default",
+        },
+      },
+      strategy: {
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::car.car",
+        ],
+      },
+    },
+  },
+};
