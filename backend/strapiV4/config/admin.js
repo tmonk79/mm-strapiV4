3c909317c4164e522b623069791fb3268665df4b
Pankaj Shere <pankaj.shere@mediamonks.com>
2022-03-16 20:27:20 +0530

    init commit docker!! V4

---
+++ backend/strapiV4/config/admin.js
@@ -0,0 +1,7 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', '6bc4b80126dbfcf4d6534c95931ffd2b'),
+  },
+  url: '/',
+  serveAdminPanel: false,
+});
---
+++ backend/strapiV4/config/server.js
@@ -0,0 +1,8 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: 'https://api.dessertcorner.com',
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
